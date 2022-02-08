a478508e27e226a09e15daa495133106853f17d1
Gurpreet Singh <guppi0204@gmail.com>
2022-02-08 22:33:11 +0530

    mobile-homepage-update

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+
+        280: {
             items:1,
-            nav:true
+            nav: true
         },
-        600:{
-            items:3,
-            nav:true
+
+
+        480: {
+            items:2,
+            nav: true
         },
-        1000:{
-            items:4,
-            nav:true,
+
+
+        768: {
+            items: 3,
+            nav: true
+        },
+
+
+        1000: {
+            items: 4,
+            nav: true,
 
         }
     }
