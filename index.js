f56c2dc184cc2557179ecf359ef36e9035fa10b8
Gurpreet Singh <guppi0204@gmail.com>
2022-02-23 12:35:55 +0530

    updated cards on homepage

--- index.js
+++ index.js
@@ -11,18 +11,39 @@ const card=document.querySelectorAll(".card__inner").forEach( card => card.addEv
 // scroll reveal
 ScrollReveal({
     reset:true,
-    distance:'60px',
+    distance:'70px',
     duration:2500,
     delay:200
 });
 
-ScrollReveal().reveal('.hero-pg-text,.btn-group, .about-heading',{delay:200, origin:'left'});
-ScrollReveal().reveal('.text-box,.Introducing-Speakers-heading ');
-ScrollReveal().reveal('.Hero-page-img',{delay:200, origin:'right'});
+ScrollReveal().reveal('.hero-pg-text,.btn-group, .about-heading,.owl-carousel',{delay:200, origin:'left'});
+ScrollReveal().reveal('.text-box,.Introducing-Speakers-heading');
+ScrollReveal().reveal('.Hero-page-img ',{delay:200, origin:'right'});
 
 
 
 
 
 // Particle Background
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+        items:3
+      }
+  }
+})
+
+
+
+
 
