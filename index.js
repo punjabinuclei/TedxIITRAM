41ad99a4f581facfb7e00aa88e9b58c75d4caf93
Gurpreet Singh <guppi0204@gmail.com>
2022-03-12 19:39:39 +0530

    home-page

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ const card=document.querySelectorAll(".card__inner").forEach( card => card.addEv
 
 
 // scroll reveal
-ScrollReveal({
-    reset:true,
-    distance:'70px',
-    duration:2500,
-    delay:200
-});
+// ScrollReveal({
+//     reset:true,
+//     distance:'70px',
+//     duration:2500,
+//     delay:200
+// });
 
-ScrollReveal().reveal('.hero-pg-text,.btn-group, .about-heading,.owl-carousel,.footer-section ',{delay:200, origin:'left'});
-ScrollReveal().reveal('.hero-pg-text,.btn-group, .about-heading,.owl-carousel, .card-body',{delay:400, origin:'left'});
-ScrollReveal().reveal('.text-box,.Introducing-Speakers-heading, .btn-map, .sponser-head, .square ', {delay:600});
-ScrollReveal().reveal('.Hero-page-img, .sponsers-logo, .map ',{delay:500, origin:'right'});
+// ScrollReveal().reveal('.hero-pg-text,.btn-group, .about-heading,.owl-carousel,.footer-section ',{delay:200, origin:'left'});
+// ScrollReveal().reveal('.hero-pg-text,.btn-group, .about-heading,.owl-carousel, .card-body',{delay:400, origin:'left'});
+// ScrollReveal().reveal('.text-box,.Introducing-Speakers-heading, .btn-map, .sponser-head, .square ', {delay:600});
+// ScrollReveal().reveal('.Hero-page-img, .sponsers-logo, .map ',{delay:500, origin:'right'});
 
 
 
@@ -30,7 +30,6 @@ ScrollReveal().reveal('.Hero-page-img, .sponsers-logo, .map ',{delay:500, origin
 $('.owl-carousel').owlCarousel({
   loop:true,
   margin:10,
-  nav:true,
   responsive:{
       0:{
           items:1
