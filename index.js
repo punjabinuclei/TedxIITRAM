d46f4f534dee859c17d947b998ab6f358c2bb8e6
Gurpreet Singh <guppi0204@gmail.com>
2022-03-19 01:02:23 +0530

    home-page

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ const card=document.querySelectorAll(".card__inner").forEach( card => card.addEv
 
 $('.owl-carousel').owlCarousel({
   loop:true,
-  margin:10,
   responsive:{
       0:{
           items:1
@@ -38,12 +37,52 @@ $('.owl-carousel').owlCarousel({
           items:2
       },
       1000:{
-        items:5
+        items:4
       }
   }
 })
 
 
+
+const slider = document.querySelector(".items");
+		const slides = document.querySelectorAll(".item");
+		const button = document.querySelectorAll(".button");
+
+		let current = 0;
+		let prev = 4;
+		let next = 1;
+
+		for (let i = 0; i < button.length; i++) {
+			button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
+		}
+
+		const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+
+		const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
+
+		const gotoNum = number => {
+			current = number;
+			prev = current - 1;
+			next = current + 1;
+
+			for (let i = 0; i < slides.length; i++) {
+				slides[i].classList.remove("active");
+				slides[i].classList.remove("prev");
+				slides[i].classList.remove("next");
+			}
+
+			if (next == 5) {
+				next = 0;
+			}
+
+			if (prev == -1) {
+				prev = 4;
+			}
+
+			slides[current].classList.add("active");
+			slides[prev].classList.add("prev");
+			slides[next].classList.add("next");
+		}
 
 
 
