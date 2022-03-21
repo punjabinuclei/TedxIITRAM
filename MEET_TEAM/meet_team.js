ab863d18f2cffde95c9fb31ff179da404d82cce7
Gurpreet Singh <guppi0204@gmail.com>
2022-03-22 00:48:00 +0530

    meet-the-team-updated

---
+++ MEET_TEAM/meet_team.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
