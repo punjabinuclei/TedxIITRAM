2db5d73705d14117904c45edb952e0582ee6632d
Gurpreet Singh <guppi0204@gmail.com>
2022-03-25 11:35:22 +0530

    meet the team page

--- MEET_TEAM/meet_team.js
+++ MEET_TEAM/meet_team.js
@@ -1,17 +1,18 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:3
-        }
-    }
-})
 
+// tabs
+
+
+(function($, document) {
+    
+    // get tallest tab__content element
+    let height = -1;
+
+      $('.tab__content').each(function() {
+          height = height > $(this).outerHeight() ? height : $(this).outerHeight();
+       $(this).css('position', 'absolute');
+      });
+    
+    // set height of tabs + top offset
+      $('[data-tabs]').css('min-height', height + 40 + 'px');
+ 
+}(jQuery, document));
