ff94361e85228dcf52597515cd33c09dbe575e9a
tedxiitram <tedxiitram@tedxiitram.ac.in>
2022-03-31 15:21:24 +0530

    home-page-edits

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ const card = document.querySelectorAll(".card__inner").forEach(card => card.addE
 
 
 
-// scroll reveal
-// ScrollReveal({
-//     reset:true,
-//     distance:'70px',
-//     duration:2500,
-//     delay:200
-// });
 
-// ScrollReveal().reveal('.hero-pg-text,.btn-group, .about-heading,.owl-carousel,.footer-section ',{delay:200, origin:'left'});
-// ScrollReveal().reveal('.hero-pg-text,.btn-group, .about-heading,.owl-carousel, .card-body',{delay:400, origin:'left'});
-// ScrollReveal().reveal('.text-box,.Introducing-Speakers-heading, .btn-map, .sponser-head, .square ', {delay:600});
-// ScrollReveal().reveal('.Hero-page-img, .sponsers-logo, .map ',{delay:500, origin:'right'});
+ScrollReveal({
+    reset:true,
+    distance:'70px',
+    duration:2500,
+    delay:200
+});
+
+ScrollReveal().reveal('.hero-pg-text,.btn-group, .about-heading,.owl-carousel,.footer-section ',{delay:200, origin:'left'});
+ScrollReveal().reveal('.hero-pg-text,.btn-group, .about-heading,.owl-carousel, .card-body',{delay:400, origin:'left'});
+ScrollReveal().reveal('.text-box,.Introducing-Speakers-heading, .btn-map, .sponser-head, .square, .speakers-heading  ', {delay:600});
+ScrollReveal().reveal('.Hero-page-img, .sponsers-logo, .map',{delay:500, origin:'right'});
 
 
 
