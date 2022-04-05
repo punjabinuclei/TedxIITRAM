57427fe1b57e89da986f9015fbeea53a59a4091c
tedxiitram <tedxiitram@tedxiitram.ac.in>
2022-04-05 21:53:25 +0530

    speaker-social-links-updated

--- index.js
+++ index.js
@@ -18,47 +18,6 @@ ScrollReveal().reveal('.speakers-heading, .about-heading, .line, .map-registrati
 ScrollReveal().reveal('.squares', {  });
 
 
-const slider = document.querySelector(".items");
-const slides = document.querySelectorAll(".item");
-const button = document.querySelectorAll(".button");
-
-let current = 0;
-let prev = 4;
-let next = 1;
-
-for (let i = 0; i < button.length; i++) {
-	button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
-}
-
-const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
-
-const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
-
-const gotoNum = number => {
-	current = number;
-	prev = current - 1;
-	next = current + 1;
-
-	for (let i = 0; i < slides.length; i++) {
-		slides[i].classList.remove("active");
-		slides[i].classList.remove("prev");
-		slides[i].classList.remove("next");
-	}
-
-	if (next == 5) {
-		next = 0;
-	}
-
-	if (prev == -1) {
-		prev = 4;
-	}
-
-	slides[current].classList.add("active");
-	slides[prev].classList.add("prev");
-	slides[next].classList.add("next");
-}
-
-
 
 
 
@@ -68,7 +27,10 @@ map.on('load', function () {
     map.resize();
 });
 
-// 
+
+
+
+// Btn------------------------------
 
 show = () => {
 
